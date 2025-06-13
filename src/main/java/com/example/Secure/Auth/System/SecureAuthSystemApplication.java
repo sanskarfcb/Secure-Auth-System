ad7a82e256758cf544283bfb74944d1c36f81c01
sanskarfcb <sanskarshashwat.vns@gmail.com>
2025-06-13 12:05:12 +0530

    Initial commit

---
+++ src/main/java/com/example/Secure/Auth/System/SecureAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Secure.Auth.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAuthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAuthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Secure/Auth/System/SecureAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Secure.Auth.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
